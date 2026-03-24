da1626ac1ac5517280cc690b08e32487cf5ca1f7
Ivan Ramanchuk <s33296@pjwstk.edu.pl>
2026-03-24 21:09:38 +0100

    Finished Policies directory

---
+++ apbd-cw3-s33296/policies/DefaultRentalPolicy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using apbd_cw3_s33296.domain;
+
+namespace apbd_cw3_s33296.policies;
+
+public class DefaultRentalPolicy : IUserLimit, ILateFee
+{
+    private const int StudentLimit = 2;
+    private const int EmployeeLimit = 5;
+    private const decimal FeePerLateDay = 15m;
+
+    public int GetRentalLimit(User user)
+    {
+        return user switch
+        {
+            Student => StudentLimit,
+            Employee => EmployeeLimit,
+            _ => throw new InvalidOperationException("Nieznany typ użytkownika")
+        };
+    }
+
+    public decimal CalculateLateFee(DateTime dueDate, DateTime returnDate)
+    {
+        var lateDays = (returnDate.Date - dueDate.Date).Days;
+        return lateDays > 0 ? lateDays * FeePerLateDay : 0m;
+    }
+}
---
+++ apbd-cw3-s33296/policies/ILateFee.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace apbd_cw3_s33296.policies;
+
+public interface ILateFee
+{
+    decimal CalculateLateFee(DateTime dueDate, DateTime returnDate);
+}
